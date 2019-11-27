e6550b31de0b3a8ecba04b21cbf1cc8253734475
Shelton <dellashelton@rockconnections.com>
2019-11-27 14:57:18 -0500

    bones

---
+++ Message Encoder/Message Encoder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Message_Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the message you would like coded: ");
+            string input = Console.ReadLine().ToUpper();
+            Console.WriteLine(input);
+                      
+            System.Text.UTF32Encoding encoding = new System.Text.UTF32Encoding();
+            byte[] bytes = encoding.GetBytes(input);
+
+            for (int i = 0; i < bytes.Length; i += sizeof(int))
+            {
+                Console.WriteLine(BitConverter.ToInt32(bytes, i-64));
+            }
+
+
+            //string message = "Your encoded message is: " + Code(input);
+
+            double checkSum;
+
+        }
+        public void Code(string input)
+        {
+
+        }
+
+    }
+}
